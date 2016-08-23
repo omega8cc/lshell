ed704ae1945b57d0749797ff55aa3027eb2fb9e2
Ignace Mouzannar <ignace@redhat.com>
2016-08-23 17:08:22 -0400

    [security] parse quoted strings for possible commands (Closes #148, Closes #147)
    Both issues #148 and #147 use the same vulnerability in the parser,
    that ignored the quoted strings. Parsing only the rest of the line
    for security issues. This is a major security bug.
    
    Thank you Proskurin Kirill (@Oloremo) and Vladislav Yarmak (@Snawoot)
    for reporting this!!

--- lshell/sec.py
+++ lshell/sec.py
@@ -174,10 +174,6 @@ def check_secure(line, conf, strict=None, ssh=None):
             ret_check_path, conf = check_path(item, conf, strict=strict)
             returncode += ret_check_path
 
-    # ignore quoted text
-    line = re.sub(r'\"(.+?)\"', '', line)
-    line = re.sub(r'\'(.+?)\'', '', line)
-
     if re.findall('[:cntrl:].*\n', line):
         ret, conf = warn_count('syntax',
                                oline,
